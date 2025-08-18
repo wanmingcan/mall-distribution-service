a55696235c5de7c02cb905997e086fe2236f6072
wanmingcan <1145709549@qq.com>
2025-08-18 15:56:51 +0800

    refactor(mall-distribution-aplication): 修改了违反结构单一性原则的部分代码
    修改了违反结构单一性原则的部分代码

--- mall-distribution-application/src/main/java/com/mall/distribution/application/service/CommissionCashCreateService.java
+++ mall-distribution-application/src/main/java/com/mall/distribution/application/service/CommissionCashCreateService.java
@@ -3,7 +3,7 @@ package com.mall.distribution.application.service;
 import com.mall.distribution.application.command.CreateCommissionCashCommand;
 import com.mall.distribution.application.port.CommissionCashCreateUseCase;
 
-public class CommissionCashCreateUseCaseService implements CommissionCashCreateUseCase {
+public class CommissionCashCreateService implements CommissionCashCreateUseCase {
     @Override
     public Long createCommissionCash(CreateCommissionCashCommand command) {
         return null;
