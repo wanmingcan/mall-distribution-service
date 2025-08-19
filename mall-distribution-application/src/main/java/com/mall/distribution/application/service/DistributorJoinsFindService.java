e701aabca900f5bd7c6a9fe19e8a5ac61e4438db
wanmingcan <1145709549@qq.com>
2025-08-19 09:51:34 +0800

    修改了文档注释的位置

--- mall-distribution-application/src/main/java/com/mall/distribution/application/service/DistributorJoinsFindService.java
+++ mall-distribution-application/src/main/java/com/mall/distribution/application/service/DistributorJoinsFindService.java
@@ -4,7 +4,6 @@ import com.mall.distribution.application.port.DistributorJoinsFindUseCase;
 import com.mall.distribution.application.query.DistributorJoinQuery;
 import com.mall.distribution.domain.model.join.DistributorJoin;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 /**
  * 查询申请列表应用服务
